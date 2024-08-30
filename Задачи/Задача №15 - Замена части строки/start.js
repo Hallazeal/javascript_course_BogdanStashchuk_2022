d4c76a59725e764f08f1b0e49636e5eae0890dfd
Nikita Danilov <danilov.ns@yandex.ru>
2024-08-30 20:22:06 +0300

    Задача №15 - Замена части строки

---
+++ Задачи/Задача №15 - Замена части строки/start.js
@@ -0,0 +1,17 @@
+/** ЗАДАЧА - Замена части строки
+ *
+ * 1. Создайте переменную и присвойте ей строку "Good Morning"
+ *
+ * 2. Замените "Morning" на "Evening" и присвойте результат новой переменной
+ *
+ * 3. Выведите значение второй переменной в консоль
+ *
+ */
+
+const greeting = "Good Morning";
+
+console.log(greeting);
+
+const updateGreeting = greeting.replace("Morning", "Evening");
+
+console.log(updateGreeting);
