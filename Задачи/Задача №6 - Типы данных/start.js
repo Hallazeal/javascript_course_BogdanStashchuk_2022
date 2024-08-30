abffbc04bb7bea1c1b981612eddec4fb974dca4e
Nikita Danilov <danilov.ns@yandex.ru>
2024-08-30 19:01:38 +0300

    Задача №6 - Типы данных

---
+++ Задачи/Задача №6 - Типы данных/start.js
@@ -0,0 +1,35 @@
+/** ЗАДАЧА - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ * - строка
+ * - число
+ * - логическое
+ * - null
+ * - undefined
+ * - объект
+ * - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений,
+ * используя оператор typeof(префиксный унарный оператор)
+ */
+
+const myString = "abc"; // - строка
+const myNumber = 10; // - число
+const myBoolean = true; // - логическое
+const myNull = null; // - null
+
+const myUndefined1 = undefined; // - undefined
+// или
+let myUndefined2;
+
+const myObject = {}; // - объект
+const myArray = []; // - массив
+
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
+console.log(typeof myNull);
+console.log(typeof myUndefined1);
+console.log(typeof myUndefined2);
+console.log(typeof myObject);
+console.log(typeof myArray);
